8f14e5f249210abc6f61dffbe191c5eef28ea59a
Dela Farahita Zain <zainfarahitadela@gmail.com>
2024-02-24 16:38:05 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<html>
+
+<body>
+    <h1>Selamat Datang !</h1>
+    <h2>Ini adalah tampilan halaman awal website</h2>
+    <p>Nama : Dela Farahita Zain <br>
+        NIM : 2241720058 <br>
+        Kelas : TI 2E
+    </p>
+    <a href="{{ url('/products') }}">Halaman Product</a> <br>
+    <a href="{{ url('/user/9/name/Dela') }}">Halaman User</a> <br>
+    <a href="{{ url('/sales') }}">Halaman Penjualan</a> <br>
+</body>
+
+</html>
